6f914864cc6b0ffb51747c33d74f0d42af014ef5
ymz <1316628671@qq.com>
2024-07-26 20:48:27 +0800

    Deploy to gh pages.Fix file path problem(caused by 'homepage').

--- dna-rankings/src/CSVReader.js
+++ dna-rankings/src/CSVReader.js
@@ -7,7 +7,10 @@ const CSVReader = ({ fileName }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(`/${fileName}`)
+    // 由于添加了homepage，所以需要修改fetch的路径
+    // 原本是'/DNA synthesis.csv'，现在是'/DNARankings/DNA synthesis.csv'
+    fetch(`${process.env.PUBLIC_URL}/${fileName}`)
+    // fetch(`/${fileName}`)
       .then((response) => response.text())
       .then((text) => {
         Papa.parse(text, {
--- dna-rankings/src/index.js
+++ dna-rankings/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// import { BrowserRouter } from 'react-router-dom';
+// const basename = process.env.PUBLIC_URL;
+// const basename = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : '';
+// console.log('basename:', basename);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <BrowserRouter basename={basename}> */}
+      <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
